6fe18eb5bc7b542995659c396205240250f3681e
Sam Billingham <sambillie.sb@gmail.com>
2018-03-20 18:50:26 +0000

    add project data for tiles

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,48 @@
-$.get('/data/sambillingham', (result, res) => {
-  // console.log(res, result)
-  if(res === 'success') processData(result)
+const USERNAME = 'sambillingham';
+
+utopianData(USERNAME).then( data => {
+  let uniqueProjects = getUniqueProjects(data)
+  let projectData = data.results
+  displayProjects(uniqueProjects, projectData)
 })
 
-function processData(data) {
-  let development = data.results.filter(project => project.json_metadata.type === 'development')
-  console.log(development)
-  let repos = development.map(project => project.json_metadata.repository.name)
-  let uniqueProjects = [...new Set(repos)]
-  displayProjects(uniqueProjects)
+function utopianData(username){
+  return new Promise((resolve,reject) => {
+    $.get(`/data/${username}`, (result, res) => {
+      if(res === 'success') resolve(result)
+    })
+  })
 }
 
-function displayProjects(data) {
-  data.forEach( (project) => {
-    let template = `<div class="project-card"><h2>${project}</h2></div>`
+function getUniqueProjects(data) {
+  let development = data.results.filter(project => project.json_metadata.type === 'development')
+  let repos = development.map(project => project.json_metadata.repository.name)
+  let uniqueProjects = [...new Set(repos)]
+  return uniqueProjects
+}
+
+function displayProjects(uniqueProjects, projectData) {
+  console.log(uniqueProjects, projectData)
+  uniqueProjects.forEach( (projectName) => {
+    let template = singleProjectTemplate(projectName, projectData)
     $('main').append(template)
   })
+}
+
+function singleProjectTemplate(projectName, projectData){
+  let projectPosts = projectData.filter(project => project.json_metadata.repository.name === projectName)
+  let votes = projectPosts.map(project => project.net_votes)
+  let avgVotes = projectPosts.reduce((total,post) => total + post.net_votes, 0) / projectPosts.length
+  let latestUpdate = projectPosts[0]
+  let projectURL = latestUpdate.json_metadata.repository.html_url
+  console.log(votes)
+  return template = `
+  <div class="project-card">
+
+    <h2>${projectName}</h2>
+    <h3>Latest: <a href="https://utopian.io/${latestUpdate.url}">${latestUpdate.title}</a></h3>
+    <h3>Updated: ${latestUpdate.created}</h3>
+    <h4>Update Count ${projectPosts.length}</h4>
+    <h4>Average Votes ${avgVotes}</h4>
+  </div>`
 }
